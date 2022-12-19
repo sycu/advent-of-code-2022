f428ef95556d8bf62487267f22d0655569ac6cae
Patryk Syc <patryk@edefine.pl>
2022-12-19 03:10:35 +0100

    Day15A time improvement

--- src/Tasks/Day15A.php
+++ src/Tasks/Day15A.php
@@ -15,18 +15,23 @@ class Day15A extends Task
      */
     protected function solve(array $lines): string
     {
-        $map = [];
+        $row = [];
         foreach ($lines as $line) {
             preg_match('/x=(.+), y=(.+):.*x=(.+), y=(.+)/', $line, $matches);
             [, $sx, $sy, $bx, $by] = array_map(fn (string $e) => (int) $e, $matches);
 
-            $map[$sx][$sy] = 0;
-            $map[$bx][$by] = 0;
+            if ($sy === self::ROW) {
+                $row[$sx] = 0;
+            }
 
-            $map = $this->markSignalCoverage($map, $sx, $sy, $this->distance($sx, $sy, $bx, $by));
+            if ($by === self::ROW) {
+                $row[$bx] = 0;
+            }
+
+            $row = $this->markSignalCoverage($row, $sx, $sy, $this->distance($sx, $sy, $bx, $by));
         }
 
-        return (string) array_sum(array_column($map, self::ROW));
+        return (string) array_sum($row);
     }
 
     protected function distance(int $x1, int $y1, int $x2, int $y2): int
@@ -34,13 +39,13 @@ class Day15A extends Task
         return (int) (abs($x1 - $x2) + abs($y1 - $y2));
     }
 
-    private function markSignalCoverage(array $map, int $sx, int $sy, int $level): array
+    private function markSignalCoverage(array $row, int $sx, int $sy, int $level): array
     {
         $dy = abs(self::ROW - $sy);
         for ($x = $sx - $level + $dy; $x <= $sx + $level - $dy; $x++) {
-            $map[$x][self::ROW] ??= 1;
+            $row[$x] ??= 1;
         }
 
-        return $map;
+        return $row;
     }
 }
